f91d43aa7043c532b422738970f0a436f84680b2
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-02-22 08:41:30 -0300

    tela de login shadow light mode e dark mode

---
+++ js/index.js
@@ -0,0 +1,24 @@
+let rootProp = document.documentElement.style;
+let mode = true;
+function ChangeModel() {
+    if (mode) {
+        darkmode();
+        
+    } else {
+        lightmode();
+    }
+    mode = !mode;
+}
+function lightmode() {
+    rootProp.setProperty("--backgound-color", "rgba(230, 230,230);");
+    rootProp.setProperty("--shadow1", "rgba(119, 119, 119,0.5)");
+    rootProp.setProperty("--shadow2", "rgba(225, 225, 225,0.85)");
+    rootProp.setProperty("--label-color", "rgba(0, 0, 0,0.8)");
+    
+}
+function darkmode() {
+    rootProp.setProperty("--backgound-color", "rgba(0, 0, 0);");
+    rootProp.setProperty("--shadow1", "rgba(0, 0, 0,0.5)");
+    rootProp.setProperty("--shadow2", "rgba(0, 0, 0,0.85)");
+    rootProp.setProperty("--label-color", "rgba(255, 255, 255,0.8)");
+}
